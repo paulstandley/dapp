c9121873ba9aff3022fa41852c3c2d04ae88ccd7
Paul Standley <paulstandley1972@gmail.com>
2019-09-07 11:18:17 +0100

    setup html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dapp
+
+![dapp](dapp.jpg)
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
@@ -13,13 +13,17 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            meme of the day
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
+                <header>
+                  <h1>Meme storage useing Ethereum Dapp With IPFS - Blockchain</h1>
+                </header>
+                <p>&nbsp;</p>
                 <a
                   href="http://www.dappuniversity.com/bootcamp"
                   target="_blank"
@@ -27,23 +31,17 @@ class App extends Component {
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <p>&nbsp;</p>
+                <h2>Change meame</h2>
+                <form className="form">
+                  <input type="file" />
+                  <input type="submit" />
+                </form>
               </div>
             </main>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
